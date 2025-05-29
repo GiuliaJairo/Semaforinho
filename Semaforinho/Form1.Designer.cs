f006f103994b802939eef636a20aa87eae9b0c15
Giulia Jairo <jairogiulia93@gmail.com>
2025-05-29 16:52:02 -0300

    Initial commit

---
+++ Semaforinho/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Semaforinho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLigar = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pibFoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pibFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLigar
+            // 
+            this.btnLigar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnLigar.Location = new System.Drawing.Point(308, 513);
+            this.btnLigar.Name = "btnLigar";
+            this.btnLigar.Size = new System.Drawing.Size(179, 44);
+            this.btnLigar.TabIndex = 1;
+            this.btnLigar.Text = "Ligar";
+            this.btnLigar.UseVisualStyleBackColor = true;
+            this.btnLigar.Click += new System.EventHandler(this.btnLigar_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pibFoto
+            // 
+            this.pibFoto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pibFoto.Image = global::Semaforinho.Properties.Resources.apagado;
+            this.pibFoto.Location = new System.Drawing.Point(221, 93);
+            this.pibFoto.Name = "pibFoto";
+            this.pibFoto.Size = new System.Drawing.Size(355, 391);
+            this.pibFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pibFoto.TabIndex = 0;
+            this.pibFoto.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 668);
+            this.Controls.Add(this.btnLigar);
+            this.Controls.Add(this.pibFoto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pibFoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pibFoto;
+        private System.Windows.Forms.Button btnLigar;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Semaforinho/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semaforinho
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private enum EstadoSemaforo { Verde, Amarelo, Vermelho, Apagado }
+
+        private EstadoSemaforo estadoAtual = EstadoSemaforo.Apagado;
+
+        private int tempo_verde = 30000;
+        private int tempo_amarelo = 30000;
+        private int tempo_vermelho = 30000;
+
+
+
+        private void btnLigar_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                btnLigar.Text = "Iniciar";
+                pibFoto.Image = Properties.Resources.apagado;
+                estadoAtual = EstadoSemaforo.Apagado;
+            }
+            else
+            {
+                estadoAtual = EstadoSemaforo.Vermelho;
+                pibFoto.Image= Properties.Resources.vermelho;
+                timer.Interval = tempo_vermelho;
+                timer.Start();
+                btnLigar.Text = "Parar";
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            switch (estadoAtual)
+            {
+                case EstadoSemaforo.Vermelho:
+                    estadoAtual = EstadoSemaforo.Verde;
+                    pibFoto.Image = Properties.Resources.verde;
+                    timer.Interval = tempo_verde;
+
+                    break;
+
+                case EstadoSemaforo.Verde:
+                    estadoAtual = EstadoSemaforo.Amarelo;
+                    pibFoto.Image = Properties.Resources.amarelo;
+                    timer.Interval = tempo_amarelo;
+
+                    break;
+
+                case EstadoSemaforo.Amarelo:
+                    estadoAtual = EstadoSemaforo.Amarelo;
+                    pibFoto.Image = Properties.Resources.amarelo;
+                    timer.Interval = tempo_vermelho;
+
+                    break;
+
+            }
+        }
+    }
+}
